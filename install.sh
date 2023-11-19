fabf922140a5070f9514ca6910e0f35c60428554
Jan Niklas Diercks <jndiercks@gmail.com>
2023-11-19 22:17:13 +0100

    add: inital package installation ideas

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # link tmux (oh-my-tmux)
-mv ~/.config/tmux/tmux.conf.local ~/dotfiles/.tmux.conf.local
 ln -s ~/dotfiles/.tmux.conf.local ~/.config/tmux/tmux.conf.local
 # link gitconfig
-mv ~/.gitconfig ~/dotfiles/.gitconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+# link zsh config
+ln -s ~/dotfiles/.zshrc ~/.zshrc
