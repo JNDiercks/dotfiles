7b760932c39c85144fbafe6768bbca2f4f981f52
Jan Niklas Diercks <janniklas.diercks@gmail.com>
2023-11-23 21:53:56 +0100

    add: gitignore

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 # link tmux (oh-my-tmux)
 ln -s ~/dotfiles/.tmux.conf.local ~/.config/tmux/tmux.conf.local
 # link gitconfig
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitconfig ~/.config/git/config
+# link gitignore
+ln -s ~/dotfiles/.gitignore ~/.config/git/ignore
 # link zsh config
 ln -s ~/dotfiles/.zshrc ~/.zshrc
