15b97ba199a91714f08840f576fecb9808dd6284
Jan Niklas Diercks <janniklas.diercks@gmail.com>
2023-12-11 10:34:36 +0100

    add: catppuccin colorscheme

--- init.lua
+++ init.lua
@@ -16,6 +16,19 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {}
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
+
 local opts = {}
 require("lazy").setup(plugins, opts)
+
+require("catppuccin").setup {
+  color_overrides = {
+    mocha = {
+      base = "#000000",
+      mantle = "#000000",
+      crust = "#000000",
+    },
+  },
+}
